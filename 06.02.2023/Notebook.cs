df79d9deeb12ccc3a62b8fc1edfd5b005fa5ac6e
salto <rakif2121@gmail.com>
2023-02-06 22:36:43 -0800

    notebook program

---
+++ 06.02.2023/Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._02._2023
+{
+    internal class Notebook
+    {     
+        public Notebook( string model, int price)
+        {
+            Model= model;
+            Price= price;
+
+        }
+        
+        public string Brand;
+        public string Model;
+        public int Price;
+
+        public string ShowInfo()
+        {
+            return Brand + " " + Model+ " "+ Price;
+        }
+
+
+    }
+}
---
+++ 06.02.2023/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06._02._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Notebook notebook = new Notebook("probook",2599);
+
+
+            notebook.Brand = "HP";
+            notebook.Model = "probook";
+            notebook.Price = 2599;
+
+            var showinfo = notebook.ShowInfo();
+            Console.WriteLine(showinfo);
+        }
+    }
+}
